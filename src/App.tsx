ac9794e989bbe48eecdb4c25511b42da2e6920c8
Nikita Choudhari <choudharinikita96@gmail.com>
2024-12-02 18:51:33 +0530

    Project in working condition

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <AngularElement scriptPath="/angular1/angular-bundle.js" rootElementName="app-root" />
-      <AngularElement scriptPath="/angular2/angular-bundle.js" rootElementName="app-root-two" />
+      <AngularElement scriptPath="/angular2/bundle.js" rootElementName="app-root-two" />
 
     </>
   );
